1e5fd1957eb462f3ce9d87dbccd794490d3be5c1
Axel Baudot <axel.baudot@protonmail.com>
2023-05-11 22:58:18 -0400

    Make static build, refactor chat name modal into own component

--- src/App.svelte
+++ src/App.svelte
@@ -1,42 +1,48 @@
 <script lang="ts">
-  import Router, { location, querystring, replace } from 'svelte-spa-router'
-  import { wrap } from 'svelte-spa-router/wrap'
+  import Router, { location, querystring, replace } from "svelte-spa-router";
+  import { wrap } from "svelte-spa-router/wrap";
 
-  import Navbar from './lib/Navbar.svelte'
-  import Sidebar from './lib/Sidebar.svelte'
-  import Footer from './lib/Footer.svelte'
-  import Home from './lib/Home.svelte'
-  import Chat from './lib/Chat.svelte'
-  import NewChat from './lib/NewChat.svelte'
-  import { chatsStorage, apiKeyStorage } from './lib/Storage.svelte'
+  import Navbar from "./lib/Navbar.svelte";
+  import Sidebar from "./lib/Sidebar.svelte";
+  import Footer from "./lib/Footer.svelte";
+  import Home from "./lib/Home.svelte";
+  import Chat from "./lib/Chat.svelte";
+  import NewChat from "./lib/NewChat.svelte";
+  import { chatsStorage, apiKeyStorage } from "./lib/Storage.svelte";
+
+  export const prerender = true;
 
   // Check if the API key is passed in as a "key" query parameter - if so, save it
   // Example: https://niek.github.io/chatgpt-web/#/?key=sk-...
-  const urlParams: URLSearchParams = new URLSearchParams($querystring)
-  if (urlParams.has('key')) {
-    apiKeyStorage.set(urlParams.get('key') as string)
+  const urlParams: URLSearchParams = new URLSearchParams($querystring);
+  if (urlParams.has("key")) {
+    apiKeyStorage.set(urlParams.get("key") as string);
   }
 
   // The definition of the routes with some conditions
   const routes = {
-    '/': Home,
+    "/": Home,
 
-    '/chat/new': wrap({
+    "/chat/new": wrap({
       component: NewChat,
       conditions: () => {
-        return !!$apiKeyStorage
-      }
+        return !!$apiKeyStorage;
+      },
     }),
 
-    '/chat/:chatId': wrap({
+    "/chat/:chatId": wrap({
       component: Chat,
       conditions: (detail) => {
-        return $chatsStorage.find((chat) => chat.id === parseInt(detail?.params?.chatId as string)) !== undefined
-      }
+        return (
+          $chatsStorage.find(
+            (chat) => chat.id === parseInt(detail?.params?.chatId as string)
+          ) !== undefined
+        );
+      },
     }),
 
-    '*': Home
-  }
+    "*": Home,
+  };
 </script>
 
 <Navbar />
@@ -49,7 +55,7 @@
       </div>
       <div class="column is-four-fifths" id="content">
         {#key $location}
-          <Router {routes} on:conditionsFailed={() => replace('/')}/>
+          <Router {routes} on:conditionsFailed={() => replace("/")} />
         {/key}
       </div>
     </div>
---
+++ src/lib/ChatRenameModal.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+
+  export let active = false;
+  export let initialChatName = "";
+
+  let localChatName = "";
+  let chatNameInput: HTMLInputElement;
+
+  const dispatch = createEventDispatcher();
+
+  const focusInput = () => {
+    if (chatNameInput && active) {
+      chatNameInput.focus();
+    }
+  };
+
+  $: {
+    if (active) {
+      localChatName = initialChatName;
+      focusInput();
+    }
+  }
+
+  const close = () => {
+    active = false;
+  };
+
+  const save = () => {
+    dispatch("save", localChatName);
+    active = false;
+  };
+</script>
+
+<div class={active ? "modal is-active" : "modal"}>
+  <div class="modal-background" on:click={close} />
+  <div class="modal-card">
+    <header class="modal-card-head">
+      <p class="modal-card-title">Enter a new name for this chat</p>
+    </header>
+    <section class="modal-card-body">
+      <div class="field is-horizontal">
+        <div class="field-label is-normal">
+          <label class="label" for="settings-chat-name">New name:</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <input
+              class="input"
+              type="text"
+              id="settings-chat-name"
+              bind:value={localChatName}
+              bind:this={chatNameInput}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+    <footer class="modal-card-foot">
+      <button class="button is-info" on:click={save}>Save</button>
+      <button class="button" on:click={close}>Cancel</button>
+    </footer>
+  </div>
+</div>
